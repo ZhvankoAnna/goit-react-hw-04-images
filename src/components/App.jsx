a4013d9df0b9fbda8291ee4f7852c79ed360c928
Zhvanko Anna <zhvankoann@gmail.com>
2023-02-24 09:24:54 +0200

    Refactored App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { searchImg } from 'services/getFetch';
 import Searchbar from './Searchbar/Searchbar';
 import Spinner from 'shared/components/Loader/Loader';
@@ -8,6 +8,93 @@ import { ToastContainer, toast } from 'react-toastify';
 import Modal from '../shared/components/Modal/Modal';
 import css from './app.module.css';
 
+const App = () => {
+  const [search, setSearch] = useState('');
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [total, setTotal] = useState(null);
+  const [status, setStatus] = useState('idle');
+  const [error, setError] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const [imageProps, setImageProps] = useState({});
+
+  const firstRender = useRef(true);
+
+  useEffect(() => {
+    if(firstRender.current){
+      firstRender.current = false;
+      return;
+    }
+    const getFetch = async () => {
+      try {
+        const { data } = await searchImg(search, page);
+        setImages(prevImages => [...prevImages, ...data.hits]);
+        setTotal(data.total);
+        setStatus('resolved');
+      } catch ({ error }) {
+        const errorMessage = error.data.message || 'Cannot fetch images.';
+        setError(errorMessage);
+        setStatus('rejected');
+      }
+    };
+
+    setStatus('pending');
+    getFetch();
+  }, [search, page]);
+
+  const getFormData = data => {
+    if (search !== data) {
+      setSearch(data);
+      setImages([]);
+      setPage(1);
+      setTotal(null);
+    }
+  };
+
+  const handleLoadMoreClick = () => setPage(prevPage => prevPage + 1);
+
+  const notifyError = message => {
+    toast.error(message);
+  };
+
+  const onShowModal = ({ largeImageURL, tags }) => {
+    setShowModal(true);
+    setImageProps({
+      largeImageURL,
+      tags,
+    });
+  };
+
+  const onCloseModal = () => {
+    setShowModal(false);
+    setImageProps({});
+  };
+
+  return (
+    <div className={css.app}>
+      {showModal && (
+        <Modal onClose={onCloseModal}>
+          <img src={imageProps.largeImageURL} alt={imageProps.tags} />
+        </Modal>
+      )}
+      <Searchbar onSubmit={getFormData} />
+      {status === 'pending' && <Spinner />}
+      {status === 'rejected' && notifyError(error)}
+      {total === 0 && notifyError('Cannot find images')}
+      {(status === 'resolved' || images.length > 0) && (
+        <ImageGallery images={images} showModal={onShowModal} />
+      )}
+      {Boolean(images?.length) && total >= page * 12 && (
+        <Button onClick={handleLoadMoreClick} />
+      )}
+      <ToastContainer autoClose={2500} />
+    </div>
+  );
+};
+
+export default App;
+
+/*
 export class App extends Component {
   state = {
     search: '',
@@ -107,3 +194,4 @@ export class App extends Component {
     );
   }
 }
+*/
